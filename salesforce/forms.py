5545d29b2b66c81fb0c94835b95f0fd31ffe88a8
Allison Ng <aln.ng3@gmail.com>
2016-05-09 20:12:17 -0700

    Cleaned up files, removed example Contact Form, can now search, yay!

--- salesforce/forms.py
+++ salesforce/forms.py
@@ -1,47 +1,73 @@
 from django import forms
+from collections import OrderedDict
 
 # our new form
 class SearchForm(forms.Form):
 	# DEPARTMENT = 'DEPT'
-	# ACCOUNT = 'ACCT'
+	# Account = 'ACCT'
 	# WAREHOUSE = 'WRHS'
 	# GAME = 'GAME'
 	# FINANCE = 'FINA'
 	# ADMINISTRATION = 'ADMN'
 
-	DEPARTMENT = 'Department'
-	ACCOUNT = 'Account'
-	MAILING_CITY = 'Mailing_City'
-	MAILING_COUNTRY = 'Mailing_Country'
-	FIRST_NAME = 'First_Name'
-	LAST_NAME = "Last_Name"
+	field_choices = OrderedDict()
+	field_choices['Department'] = 'Department'
+	field_choices['Account'] = 'Account'
+	field_choices['MailingCity'] = 'Mailing City'
+	field_choices['MailingCountry'] = 'Mailing Country'
+	field_choices['FirstName'] = 'First Name'
+	field_choices['LastName'] = "Last Name"
 
- 
-	FILTER_CHOICES = [
-		(DEPARTMENT, 'Department'),
-		(ACCOUNT, 'Account'),
-		(MAILING_CITY, 'City'),
-		(MAILING_COUNTRY, 'Country'),
-		(FIRST_NAME, 'First Name'),
-		(LAST_NAME, 'Last Name'),
-		]
+
+	# FILTER_CHOICES = [
+	# 	(Department, 'Department'),
+	# 	(Account, 'Account'),
+	# 	(MailingCity, 'City'),
+	# 	(MAILING_COUNTRY, 'Country'),
+	# 	(FirstName, 'First Name'),
+	# 	(LastName, 'Last Name'),
+	# 	]
 	
-	filters = forms.MultipleChoiceField(label='Choose what to filter by', 
-										choices=FILTER_CHOICES, 
-										widget=forms.CheckboxSelectMultiple,
-										required=True)
+	# filters = forms.MultipleChoiceField(label='Choose what to filter by', 
+	# 									choices=FILTER_CHOICES, 
+	# 									widget=forms.CheckboxSelectMultiple,
+	# 									required=True)
 	
-	name = forms.CharField(label='Name of filter (e.g. Department: \'Marketing\')', 
+	dept_name = forms.CharField(label=field_choices['Department'], 
 							max_length=100,
- 							required=True,
- 							initial='')
+ 							required=False,
+ 							)
+	account_name = forms.CharField(label=field_choices['Account'],
+							max_length=100,
+ 							required=False,
+ 							initial='EA Games'
+ 							)
+	city_name = forms.CharField(label=field_choices['MailingCity'],
+							max_length=100,
+							required=False,
+							)
+	country_name = forms.CharField(label=field_choices['MailingCountry'], 
+							max_length=100,
+ 							required=False,
+ 							)
+	first_name = forms.CharField(label=field_choices['FirstName'], 
+							max_length=100,
+ 							required=False,
+ 							)
+	last_name = forms.CharField(label=field_choices['LastName'], 
+							max_length=100,
+ 							required=False,
+ 							)	
 
+	# TODO 
+	# http://stackoverflow.com/questions/25457695/django-must-have-at-least-one-form-field-filled-in
 	'''
 	def clean_filters(self):
 		if len(self.cleaned_data['filters']) < 1:
 			raise forms.ValidationError('Please select at least 1')
 		return self.cleaned_data['filters']
 	'''
+
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
--- salesforce/views.py
+++ salesforce/views.py
@@ -6,19 +6,18 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 from simple_salesforce import Salesforce
+
+from collections import OrderedDict
 import config # holds confidential login infoo
 
 
 # Create your views here.
 def index(request):
-	# query_contacts = get_salesforce_data()
+	# query_contacts = query_salesforce()
 	form_class = SearchForm
-	test = "Hello creator"
 
 	context = {
-		'content': test,
 		'form': form_class,
-		# 'contact_list': query_contacts['records'],
 	}
 
 	# TODO show errors if not valid
@@ -29,89 +28,125 @@ def index(request):
 
 		if form.is_valid():
 			print "form is valid!"
-			context['filters'] = request.POST.get('filters')
-			context['name'] = request.POST.get('name')
-			print "filters chosen:"
-			print request.POST.getlist('filters')
 
-			return HttpResponseRedirect('/')
+			form_fields = OrderedDict()
+			print "dept name" + form.cleaned_data['dept_name']
+			form_fields['Department'] = form.cleaned_data['dept_name']
+			form_fields['Account'] = form.cleaned_data['account_name']
+			form_fields['MailingCity'] = form.cleaned_data['city_name']
+			form_fields['MailingCountry'] = form.cleaned_data['country_name']
+			form_fields['FirstName'] = form.cleaned_data['first_name']
+			form_fields['LastName'] = form.cleaned_data['last_name']
+
+
+			# print "filters chosen:"
+			# print 'Department ' + form_fields['Department']
+			# print 'Account: ' + form_fields['Account'] 
+			# print 'MailingCity: ' + form_fields['MailingCity']
+			# print 'MailingCountry ' + form_fields['MailingCountry']
+			# print 'FirstName ' + form_fields['FirstName']
+			# print 'LastName ' + form_fields['LastName']
+
+			query_result = query_salesforce(form_fields)
+			context['contact_list'] = parse_query_result(query_result)
+			# print 'contact list ', context['contact_list']
+
 		else:
-			print "errors: "
+			print "ERRORS in form: "
 			print form.errors
-	# return HttpResponse("Hello World!")
-	# return HttpResponse(template.render(context, request))
 	return render(request, 'salesforce/index.html', context)
 
 	#https://docs.djangoproject.com/en/1.9/intro/tutorial03/
 	# should use render() and get_http_and_404
 
-def contact(request):
-    form_class = ContactForm
+def query_salesforce(form_fields):
+	# sf being the salesforce connection
+	(isLoggedIn, sf) = salesforce_login()
 
-    print "Trying to login"
-    (isLoggedIn, sf) = salesforce_login()
-    if isLoggedIn:
-    	print "hurrah logged in check passes"
-    
-    return render(request, 'salesforce/contact.html', {
-        'form': form_class,
-    })
+	if isLoggedIn:
+		print "Grabbing salesforce data"
+		query_select = "Id, Name, Email, Account.Name"
+		query_from = 'Contact'
+		query_where = ''
+		for key, value in form_fields.items():
+			# print '\nkey ' + key + ' value ' + value
+			if value:
+				#if query_where already has value in it
 
-def salesforce_login(filters):
+				if query_where:
+					query_where += " AND "
+
+				if key == 'Account':
+					query_where += 'Account.Name=\'' + value + '\''
+				else:
+					query_where = query_where + key + \
+								'=\'' + value + '\''
+		print "query where" + query_where
+
+		query = "SELECT " + query_select + \
+			" FROM " + query_from + \
+			" WHERE " + query_where 
+		# print 'query is:\n' + query
+
+		query_result = sf.query_all(query)
+		# print "query result: ", query_result
+		# return query_result
+
+def salesforce_login():
 	try: 
 		sf = Salesforce(
 			username=config.username, 
-			# password=config.password,
-			password=config.username,
+			password=config.password,
 			security_token=config.security_token
 			)
 		print "\nLogged in\n"
 		return (True, sf)
-	# except SalesforceAuthenticationFailed: 
-	# 	print "Error logging into Salesforce"
 	# exception SalesforceAuthenticationFailed doesn't work, need to use wildcard
 	except: 
 		print "\nError logging into Salesforce\n"
 
+def parse_query_result(query_result):
+	contact_list = []
+	for obj in query_result['records']:
+		contact = OrderedDict()
+		contact['Id'] = obj['Id']
+		contact['Name'] = obj['Name']
+		contact['Email'] = obj['Email']
+		contact['AccountName'] = obj['Account']['Name']
+		print "\ncontact: ", contact
+		contact_list.append(contact)
+	return contact_list
 
-def get_salesforce_data(filters):
-	# need filter and name of filters 
-	print "username " + config.username
-
-	#
-
-	print "connected"
-	query_select = "id, FirstName, LastName, Email"
-	query_from = 'Contact'
-	query_where = 
-	for filter in filters:
-		if filter = 
-	query = "SELECT " + query_select + \
-		" FROM " + query_from + \
-		" WHERE " + query_where 
-	print 'query is:\n' + query
-
-	# return sf.query_all("SELECT id, FirstName, LastName, Email FROM Contact")
-	
 
 '''
-OrderedDict(
-	[(u'totalSize', 1), (u'done', True), 
-		(u'records',
-			[OrderedDict([
-				(u'attributes', OrderedDict([
-					(u'type', u'Contact'), (u'url',
-u'/services/data/v29.0/sobjects/Contact/0033600000CaRlaAAF')]
-					)), 
-				(u'Id', u'0033600000CaRlaAAF'), 
-				(u'FirstName', u'John')])
-			])
-	])
 
-	to get first name
+	To get First name of first contact:
 	name = query['records'][0]['firstName'] (inner ordereddict is a list)
+	[0] being first contact in list
+	[1] being 2nd...
 
-	each item in query['records'] is a contact OBJECT (first name being an attr)
+	each item in query['records'] is a contact dict (first name being an attr)
+
+	OrderedDict(
+	[(u'totalSize', 2),
+	(u'done', True),
+	(u'records', [OrderedDict([
+		(u'attributes', OrderedDict([(u'type', u'Contact'), (u'url', u'/services/data/v29.0/sobjects/Contact/0033600000DAcYGAA1')])), 
+		(u'Id', u'0033600000DAcYGAA1'), 
+		(u'Name', u'Olivia Wilde'), 
+		(u'Email', u'oliviawilde@eagames.ca'), 
+		(u'Account', OrderedDict([(u'attributes', OrderedDict([(u'type', u'Account'), (u'url', u'/services/data/v29.0/sobjects/Account/0013600000JDa19AAD')])), (u'Name', u'EA Games')])
+	  	)]), 
+		OrderedDict([
+			(u'attributes', OrderedDict([(u'type', u'Contact'), (u'url', u'/services/data/v29.0/sobjects/Contact/0033600000CaRlaAAF')])), 
+			(u'Id', u'0033600000CaRlaAAF'), 
+			(u'Name', u'John Smith'), 
+			(u'Email', u'jonsthebomb@gmail.com'), 
+			(u'Account', OrderedDict([(u'attributes', OrderedDict([(u'type', u'Account'), (u'url', u'/services/data/v29.0/sobjects/Account/0013600000JDa19AAD')])), (u'Name', u'EA Games')]))])
+		]
+	)]
+
+	)
 
 
 '''
